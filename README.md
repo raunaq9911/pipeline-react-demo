4166ee0294a47cc26d41d3e60ae7b7f26f944da0
raunaq9911 <96716337+raunaq9911@users.noreply.github.com>
2021-12-27 13:01:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started with CD
+
+A sample React application to demonstrate continous deployment using AWS 
+
+App Link: https://rb.gy/x9zv1h 
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/src/jquery";
+import "bootstrap/dist/js/bootstrap";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="box">
+        <h3>Counter</h3>
+        <br />
+        <div>
+          <h1>{count}</h1>
+          <br />
+          <div className="btnGroup">
+            <button
+              className="btn btn-success"
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              <span className="glyphicon glyphicon-plus"></span>
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                setCount(count - 1);
+              }}
+            >
+              <span className="glyphicon glyphicon-minus"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
